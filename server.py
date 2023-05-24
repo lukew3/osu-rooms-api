846752e9f271fa04b79b0a4049bc0afeb06d4572
lukew3 <lukew25073@gmail.com>
2023-05-24 09:25:07 -0400

    add flask-cors

--- server.py
+++ server.py
@@ -1,9 +1,11 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 from math import ceil
 import sqlite3
 import datetime
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route("/")
